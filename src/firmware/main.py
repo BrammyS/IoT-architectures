ae1084c0dcb1af93181d45593470f13d4be9341d
Bram <bram.esendam@gmail.com>
2023-10-17 18:48:41 +0200

    better code!

--- src/firmware/main.py
+++ src/firmware/main.py
@@ -1,10 +1,9 @@
 from network import LoRa
-import l
-import env
-import os
-import ubinascii
-import socket
-import time
+import env, time, pycom
+import socket, ubinascii
+
+pycom.rgbled(0x7f0000)
+time.sleep(1)
 
 lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
 print(ubinascii.hexlify(lora.mac()).upper().decode('utf-8'))
@@ -12,40 +11,32 @@ dev_eui = ubinascii.unhexlify(env.DEV_EUI)
 app_eui = ubinascii.unhexlify(env.APP_EUI)
 app_key = ubinascii.unhexlify(env.APP_KEY)
 
-def send_message(socket_connection, byte_array):
-    # make the socket blocking
-    # (waits for the data to be sent and for the 2 receive windows to expire)
-    socket_connection.setblocking(True)
-    # send some data
-    socket_connection.send(bytearray(byte_array))
-    # make the socket non-blocking
-    # (because if there's no data received it will block forever...)
-    socket_connection.setblocking(False)
-
 # join a network using OTAA
 lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
 
-# wait until the module has joined the network
+# Loop until joined
 while not lora.has_joined():
     time.sleep(2.5)
     print('Not yet joined...')
 
-# Setup socket connection
-socket_connection = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-socket_connection.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+print('Joined!')
 
-send_message(socket_connection, [0x41, 0x02, 0x03])
+# Create a LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
-# get any data received (if any...)
-data = socket_connection.recv(64)
-if data:
-    received_integer = int.from_bytes(data, byteorder='big', signed=False)
-    print(received_integer)
-else:
-    print("No data received")
+# Set the LoRaWAN data rate
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 
-print("Done")
-# hex_data = ubinascii.hexlify(data)
-# received_integer = int(hex_data, 16)  # Convert the hexadecimal string to an integer
-# print(received_integer)
-print("Done")
+# Make the socket non-blocking
+s.setblocking(False)
+
+# Define a function to send SenML data
+def send_data(data):
+    s.setblocking(True)
+    s.send(data)
+    s.setblocking(False)
+
+# Example of generating and sending fake sensor data
+while True:
+    send_data("86836461616161636c6174fb4049fa364388ebcc8366626262626262636c6f6efb401b003ff69014b68366636363636363616dfb409cbe261fe21d9783656464646464612519270f8366656565656565612519270fa2006766666666666666036d31363937353537323133353436")
+    time.sleep(60)  # Adjust the interval as needed
